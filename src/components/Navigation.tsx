29d93b63d6ae6a708641ad73f03e239c2cd2684f
Derrick Nguyen <derrickhnguyen@outlook.com>
2022-03-30 11:21:54 -0700

    Update

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,41 +5,39 @@ interface NavigationProps {
   active: "home" | "film" | "resume";
 }
 
-const Navigation = ({active}: NavigationProps) => {
-  return (
-    <nav className="flex justify-center items-center content-center pt-3 pb-8 uppercase">
-      <Link href="/">
-        <a
-          className={`mr-4 hover:opacity-50 focus:outline-none focus:shadow-outline ${
-            active === "home" ? "text-orange-600 underline" : ""
-          }`}
-          href="/"
-        >
-          home
-        </a>
-      </Link>
-      <Link href="/film">
-        <a
-          className={`mr-4 hover:opacity-50 focus:outline-none focus:shadow-outline ${
-            active === "film" ? "text-orange-600 underline" : ""
-          }`}
-          href="/film"
-        >
-          film
-        </a>
-      </Link>
-      <Link href="/resume">
-        <a
-          className={`hover:opacity-50 focus:outline-none focus:shadow-outline ${
-            active === "resume" ? "text-orange-600 underline" : ""
-          }`}
-          href="/resume"
-        >
-          resume
-        </a>
-      </Link>
-    </nav>
-  );
-};
+const Navigation = ({active}: NavigationProps) => (
+  <nav className="flex justify-center items-center content-center pt-3 pb-8 uppercase">
+    <Link href="/">
+      <a
+        className={`mr-4 hover:opacity-50 focus:outline-none focus:shadow-outline ${
+          active === "home" ? "text-orange-600 underline" : ""
+        }`}
+        href="/"
+      >
+        home
+      </a>
+    </Link>
+    <Link href="/film">
+      <a
+        className={`mr-4 hover:opacity-50 focus:outline-none focus:shadow-outline ${
+          active === "film" ? "text-orange-600 underline" : ""
+        }`}
+        href="/film"
+      >
+        film
+      </a>
+    </Link>
+    <Link href="/resume">
+      <a
+        className={`hover:opacity-50 focus:outline-none focus:shadow-outline ${
+          active === "resume" ? "text-orange-600 underline" : ""
+        }`}
+        href="/resume"
+      >
+        resume
+      </a>
+    </Link>
+  </nav>
+);
 
 export default Navigation;
